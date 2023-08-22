54164680c5720635cd14f72b512d6d6dce36c21c
Guilherme Matos <guimvmatos1@outlook.com>
2023-08-22 16:27:01 -0300

    Update l3-load-latency5gIp.lua

--- examples/l3-load-latency5gIp.lua
+++ examples/l3-load-latency5gIp.lua
@@ -8,6 +8,7 @@ local stats  = require "stats"
 local timer  = require "timer"
 local arp    = require "proto.arp"
 local log    = require "log"
+local pcap = require "pcap"
 
 -- set addresses here
 local DST_MAC		= "ac:1f:6b:67:06:41"
